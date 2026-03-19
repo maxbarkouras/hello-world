c3ab258d71ba21690d8432794b76cd29a17e121e
maxbarkouras <max@computer>
2026-03-19 13:59:12 -0700

    x64 asm

--- hello-world.asm
+++ hello-world.asm
@@ -8,13 +8,20 @@ section .text
 global _start
 
 _start:
-
     mov rax, 1
     mov rdi, 1
     mov rsi, helloWorld
-    mov rdx, 14
+    mov rdx, 12
     syscall
+
+    cmp rax, 12
+    jne failed
 
     mov rax, 60
     mov rdi, 0
+    syscall
+
+failed:
+    mov rdi, rax
+    mov rax, 60
     syscall
