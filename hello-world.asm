c6acc6e81450a315aa857dd3bdaf91be73db6b6a
maxbarkouras <max@computer>
2026-03-19 13:52:38 -0700

    x64 asm

---
+++ hello-world.asm
@@ -0,0 +1,20 @@
+; x64 asm hello world
+BITS 64
+
+section .data
+    helloWorld db "hello world", 0x0a, 0x00 
+
+section .text
+global _start
+
+_start:
+
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, helloWorld
+    mov rdx, 14
+    syscall
+
+    mov rax, 60
+    mov rdi, 0
+    syscall
